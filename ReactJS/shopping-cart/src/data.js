f6bc8c42010091d833262ae327081bd1ac191859
S.Anandu <anandusr360@gmail.com>
2022-01-11 18:47:36 +0530

    react

---
+++ ReactJS/shopping-cart/src/data.js
@@ -0,0 +1,40 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+import img4 from './img/img4.png';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'MSI GS66',
+            desc:'',
+            price: 280000,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Asus ROG Zephyrus Duo',
+            desc:'',
+            price: 229000,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Razer Blade 15',
+            desc:'',
+            price: 240000,
+        },
+        {
+            id: 4,
+            img: img4,
+            title: 'Gigabyte Aero 15',
+            desc:'',
+            price: 120000,
+        }
+
+    ],
+};
+
+export default data;
